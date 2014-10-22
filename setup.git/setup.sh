5bff6afa91221905f0037687c3be13d1d4a15869
seigneur <contact.dias@gmail.com>
2014-10-22 18:42:11 +0800

    Update setup.sh
    Removed Heroku and added latest way to install Node Js.

--- setup.git/setup.sh
+++ setup.git/setup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
-# for headless setup. 
+# Simple setup.sh for configuring Ubuntu 14 witn Node js alongwith emacs Repl
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
+
+#Install Node js
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -27,9 +23,10 @@ sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
+# Don't want Heroku
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
